1ca8b164b49f57dfe149744742247ecce41057f6
Hamit SEYREK <hamitseyrek@gmail.com>
2021-10-12 16:20:02 +0300

    next question process cancel edit for normal quiz

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -17,6 +17,22 @@ class AjaxController extends Controller
 
         switch ($type) {
 
+            case 'get-questions' :
+                $questions = SurveyQuestions::with('getOptions')->get();
+                //$options = SurveyQuestionOptions::where('question_id', $ques->id)->get();
+                $array = [];
+                for ($i = 0; $i < count($questions); $i++) {
+                    if (count($questions[$i]->getOptions) == 4) {
+                        array_push($array, array('questionId' => $questions[$i]->id, 'question' => $questions[$i]->question, 'cho0' => $questions[$i]->getOptions[0]['option_value'], 'cho1' => $questions[$i]->getOptions[1]['option_value'], 'cho2' => $questions[$i]->getOptions[2]['option_value'], 'cho3' => $questions[$i]->getOptions[3]['option_value']));
+                    } elseif (count($questions[$i]->getOptions) == 3) {
+                        array_push($array, array('questionId' => $questions[$i]->id, 'question' => $questions[$i]->question, 'cho0' => $questions[$i]->getOptions[0]['option_value'], 'cho1' => $questions[$i]->getOptions[1]['option_value'], 'cho2' => $questions[$i]->getOptions[2]['option_value']));
+                    } else {
+                        array_push($array, array('questionId' => $questions[$i]->id, 'question' => $questions[$i]->question, 'cho0' => $questions[$i]->getOptions[0]['option_value'], 'cho1' => $questions[$i]->getOptions[1]['option_value']));
+                    }
+                }
+                echo json_encode($array);
+                break;
+
             case 'get-question' :
                 $id = $request->input('id');
                 $ques = SurveyQuestions::find($id);
@@ -32,20 +48,27 @@ class AjaxController extends Controller
                 break;
 
             case 'next-question' :
+                $array = [];
                 $choise = $request->input('choise');
                 $id = $request->input('questionId');
-                $findOptId = SurveyQuestionOptions::where('question_id', $id)->where('option_value', $choise)->first()->id;
-                $ques = SurveyQuestionSqoptions::find($findOptId);
-                $options = SurveyQuestionOptions::where('question_id', $ques->id)->get();
+                $findOpt = SurveyQuestionOptions::where('option_value', $choise)->first();
+                $findQuestion = SurveyQuestionSqoptions::where('option_id', $findOpt->id)->first();
+                if ($findQuestion != null) {
 
-                if (isEmpty($options[3])) {
-                    $array = array('questionId' => $ques->id, 'question' => $ques->question, 'cho0' => $options[0]['option_value'], 'cho1' => $options[1]['option_value'], 'cho2' => $options[2]['option_value'], 'cho3' => $options[3]['option_value']);
-                } elseif (isEmpty($options[2])) {
-                    $array = array('questionId' => $ques->id, 'question' => $ques->question, 'cho0' => $options[0]['option_value'], 'cho1' => $options[1]['option_value'], 'cho2' => $options[2]['option_value']);
+                    $ques = SurveyQuestions::find($findQuestion->question_id);
+                    $options = SurveyQuestionOptions::where('question_id', $ques->id)->get();
+
+                    if (isEmpty($options[3])) {
+                        $array = array('questionId' => $ques->id, 'question' => $ques->question, 'cho0' => $options[0]['option_value'], 'cho1' => $options[1]['option_value'], 'cho2' => $options[2]['option_value'], 'cho3' => $options[3]['option_value']);
+                    } elseif (isEmpty($options[2])) {
+                        $array = array('questionId' => $ques->id, 'question' => $ques->question, 'cho0' => $options[0]['option_value'], 'cho1' => $options[1]['option_value'], 'cho2' => $options[2]['option_value']);
+                    } else {
+                        $array = array('questionId' => $ques->id, 'question' => $ques->question, 'cho0' => $options[0]['option_value'], 'cho1' => $options[1]['option_value']);
+                    }
+                    echo json_encode($array);
                 } else {
-                    $array = array('questionId' => $ques->id, 'question' => $ques->question, 'cho0' => $options[0]['option_value'], 'cho1' => $options[1]['option_value']);
+                    echo json_encode($array);
                 }
-                echo json_encode($array);
                 break;
         }
     }
--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -9,7 +9,7 @@
 <body>
 <div class="grid">
     <div id="quiz">
-        <h1 style="background-color: #1D3CFA">Quiz in JavaScript birmuhendis.net</h1>
+        <h1 style="background-color: #ffffff; color: #0e5ab4">Quiz in JavaScript birmuhendis.net</h1>
         <hr style="margin-bottom: 20px">
 
         <p id="question"></p>
@@ -19,38 +19,16 @@
             <button id="btn1"><span id="choice1"></span></button>
             <button id="btn2"><span id="choice2"></span></button>
             <button id="btn3"><span id="choice3"></span></button>
-
         </div>
         <hr style="margin-top: 50px">
         <footer>
-            <p id="progress">Question x of y</p>
+            <p id="progress"></p>
         </footer>
     </div>
 </div>
 
 
 <script>
-    function Quiz(questions) {
-        this.score = 0;
-        this.questions = questions;
-        this.questionIndex = 0;
-    }
-
-    Quiz.prototype.getQuestionIndex = function () {
-        return this.questions[this.questionIndex];
-    }
-
-    Quiz.prototype.guess = function (answer) {
-        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
-            this.score++;
-        }
-        this.questionIndex++;
-    }
-
-    Quiz.prototype.isEnded = function () {
-        return this.questionIndex === this.questions.length;
-    }
-
 
     function Question(text, choices, questionId) {
         this.text = text;
@@ -58,18 +36,36 @@
         this.questionId = questionId;
     }
 
-    Question.prototype.isCorrectAnswer = function (choice) {
-        $.post('{{url('/ajax')}}', {
-            _token: '{{csrf_token()}}',
-            type: 'next-question',
-            choise: choice,
-            questionId: this.questionId,
-        }, function abc(ret) {
-            var item = new Question(ret['question'],  [(ret['cho0'] != null) ? ret['cho0'] : "", (ret['cho1'] != null) ? ret['cho1'] : "",(ret['cho2'] != null) ? ret['cho2'] : "",(ret['cho3'] != null) ? ret['cho3'] : ""], ret['questionId']);
-            questions.push(item);
-        }, "json");
-        return true;
-        //return this.answer === choice;
+    var questions = [];
+    var item2 = new Question("İlk Soru?", ["PHP", "HTML", "JS", "refd"], 1);
+    questions.push(item2);
+
+    var choiseName = "";
+    var quId = 0;
+
+    // create quiz
+    var quiz = new Quiz(questions);
+
+    // display quiz
+    function Quiz(questions) {
+        // this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+
+    Quiz.prototype.getQuestionIndex = function () {
+        return this.questions[this.questionIndex];
+    }
+
+    Quiz.prototype.isEnded = function () {
+        return this.questionIndex === this.questions.length;
+    } // gelen son id geçerli mi değil mi ona bakılabilir
+
+
+    Quiz.prototype.guess = function (answer) {
+
+        this.questionIndex++;
     }
 
 
@@ -84,20 +80,64 @@
             // show options
             var choices = quiz.getQuestionIndex().choices;
             var qId = quiz.getQuestionIndex().questionId;
+            console.log(choices.length);
             for (var i = 0; i < choices.length; i++) {
                 var element = document.getElementById("choice" + i);
                 element.innerHTML = choices[i];
-                guess("btn" + i, choices[i]);
-            }
 
+                guess("btn" + i, choices[i], qId);
+            }
             showProgress();
         }
     };
+    $.post('{{url('/ajax')}}', {
+        _token: '{{csrf_token()}}',
+        type: 'get-questions',
+    }, function abc(ret) {
+        if (ret != null) {
+            for (var i = 0; i < ret.length; i++) {
+                var item = new Question(ret[i]['question'],
+                    [(ret[i]['cho0'] != null) ? ret[i]['cho0'] : "", (ret[i]['cho1'] != null) ? ret[i]['cho1'] : "", (ret[i]['cho2'] != null) ? ret[i]['cho2'] : "", (ret[i]['cho3'] != null) ? ret[i]['cho3'] : ""],
 
-    function guess(id, guess) {
+                    /*(ret[i]['cho3'] != null) ?
+                        [(ret[i]['cho0'] != null) ? ret[i]['cho0'] : "", (ret[i]['cho1'] != null) ? ret[i]['cho1'] : "", (ret[i]['cho2'] != null) ? ret[i]['cho2'] : "", (ret[i]['cho3'] != null) ? ret[i]['cho3'] : ""] :
+                        (ret[i]['cho2'] != null) ?
+                            [(ret[i]['cho0'] != null) ? ret[i]['cho0'] : "", (ret[i]['cho1'] != null) ? ret[i]['cho1'] : "", (ret[i]['cho2'] != null) ? ret[i]['cho2'] : ""] :
+                            [(ret[i]['cho0'] != null) ? ret[i]['cho0'] : "", (ret[i]['cho1'] != null) ? ret[i]['cho1'] : ""],*/
+                    ret[i]['questionId']);
+                questions.push(item);
+            }
+            this.questions = questions;
+        }
+    }, "json");
+
+    function guess(id, guess, quId) {
+        choiseName = guess;
+        //console.log(quId);
+        //console.log(choiseName);
+        /*$.post('{{url('/ajax')}}', {
+            _token: '{{csrf_token()}}',
+            type: 'next-question',
+            choise: choiseName,
+        }, function abc(ret) {
+            if(ret['question'] != null){
+            var item = new Question(ret['question'], [(ret['cho0'] != null) ? ret['cho0'] : "", (ret['cho1'] != null) ? ret['cho1'] : "", (ret['cho2'] != null) ? ret['cho2'] : "", (ret['cho3'] != null) ? ret['cho3'] : ""], ret['questionId']);
+            questions.push(item);
+            this.questions = questions;
+            //console.log(quiz);
+            question_id = quId;
+            }
+        }, "json");*/
         var button = document.getElementById(id);
+        if (guess == '') {
+            button.style.visibility = "hidden";
+        }else{
+            button.style.visibility = "";
+
+        }
         button.onclick = function () {
             quiz.guess(guess);
+            console.log(guess)
             populate();
         }
     };
@@ -117,32 +157,8 @@
     };
 
     // create questions here
-    var question_id = 1;
-    var choiseName = "";
 
-    var questions = [];
 
-    while (question_id != null) {
-        $.post('{{url('/ajax')}}', {
-            _token: '{{csrf_token()}}',
-            type: 'get-question',
-            id: question_id
-        }, function abc(ret) {
-            var item = new Question(ret['question'],  [(ret['cho0'] != null) ? ret['cho0'] : "", (ret['cho1'] != null) ? ret['cho1'] : "",(ret['cho2'] != null) ? ret['cho2'] : "",(ret['cho3'] != null) ? ret['cho3'] : ""], ret['questionId']);
-            questions.push(item);
-        }, "json");
-
-        var item2 = new Question("İlk Soru?", ["PHP", "HTML", "JS", "refd"], 1);
-        questions.push(item2);
-        question_id = null;
-    }
-function getnextQuestion(question_id) {
-
-}
-    // create quiz
-    var quiz = new Quiz(questions);
-
-    // display quiz
     populate();
 </script>
 
